cc3a3e0856fa7c7e543f87335a8b6fe29ef0c052
Ibrahim-Madni <ibrahim.madni@hotmail.com>
2022-11-14 03:55:34 +0500

    Second commit

---
+++ test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import React from 'react'
+
+function test() {
+  return (
+    <div>test</div>
+  )
+}
+
+function App(){
+
+return <div className="test">hello world</div>
+}
+
+export default {App, test};
